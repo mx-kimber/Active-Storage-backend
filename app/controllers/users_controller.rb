dfd9c52166aace7a4b9e2f4fdead54edceba1211
Kimber Ogden <mx.kimber@gmail.com>
2023-10-29 19:47:16 -0500

    Association: add 'has_one_attached :avatar' to the user model, permit :avatar param in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -60,8 +60,9 @@ class UsersController < ApplicationController
   def latest
     @user = user.last
     render json: @user
+
   end
-  private
+    private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = User.find(params[:id])
@@ -69,6 +70,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.fetch(:user, {})
+      params.require(:user).permit(:avatar)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+
+    has_one_attached :avatar
+
 end
