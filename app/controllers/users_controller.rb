d7e8f9555ac5f00150012cdc52f0589d2ea7bbbd
Kimber Ogden <mx.kimber@gmail.com>
2023-10-29 19:39:48 -0500

    Initialize Cors (create new file 'Cors.rb' and bundle the cors gem / add action method to Users controller to call the latest user / add route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -57,6 +57,10 @@ class UsersController < ApplicationController
     end
   end
 
+  def latest
+    @user = user.last
+    render json: @user
+  end
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
