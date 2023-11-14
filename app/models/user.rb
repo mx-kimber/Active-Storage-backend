760c509ff42b8f5ccb059c18f77eb11abfa83808
Kimber Ogden <mx.kimber@gmail.com>
2023-11-13 21:52:01 -0600

    imstall active_text ( rails g action_text:install ), migrate, and bundle.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ApplicationRecord
 
     has_one_attached :avatar
+  # has_rich_text :body
 
-      def avatar_url
-        Rails.application.routes.url_helpers.url_for(avatar) if avatar.attached?
-      end
-        
+  # Note: I am not using rich-text at this current time. It is not needed, but I may come back to this in future learning.
+
+    def avatar_url
+      Rails.application.routes.url_helpers.url_for(avatar) if avatar.attached?
+    end
+      
 
 end
